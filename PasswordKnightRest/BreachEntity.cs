b797e4d3505cd40fc10ffc0467773be6883fd4b2
pavan <pavanvarmausa@gmail.com>
2018-06-28 03:04:34 -0500

    Added passwords and domains endpoints

---
+++ PasswordKnightRest/BreachEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordKnightRest
+{
+    public class BreachEntity : TableEntity
+    {
+        public BreachEntity() { }
+
+        public BreachEntity(string breachName, string email, string domain)
+        {
+            Domain = domain;
+            PartitionKey = breachName;
+            RowKey = email;
+        }
+
+        public String Domain;
+        public String Breach => PartitionKey;
+        public String Email => RowKey;
+    }
+}
---
+++ PasswordKnightRest/Controllers/DomainsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PasswordKnightRest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Domains")]
+    public class DomainsController : Controller
+    {
+        // GET: api/Domains
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Domains/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(String id)
+        {
+            CloudStorageAccount storageAccount =
+                  CloudStorageAccount.
+              Parse("DefaultEndpointsProtocol=https;AccountName=appserversdiag232;AccountKey=yXX7DE/aWLwQFTtx9Pc1PXRLs+iv2RSDT9lowMQqh9O2T8DwIaB1DRjuTeBp9x8uWPTx61sy5rMI+rWUyqj/rA ==");
+
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            // Get a reference to a table named "peopleTable".
+            CloudTable cloudTable = tableClient.GetTableReference("ClearTextPasswordsTable");
+
+            //TableQuery<DynamicTableEntity> projectionQuery = new TableQuery<DynamicTableEntity>().Select(new string[] { "Email" });
+
+            //EntityResolver<string> resolver = (pk, rk, ts, props, etag) => props.ContainsKey("Email") ? props["Email"].StringValue : null;
+
+            //var result = cloudTable.
+            String response = "";
+
+
+            var retrievedResult = cloudTable.ExecuteAsync(TableOperation.Retrieve("password",
+                                                                              id));
+            if (retrievedResult.Result.HttpStatusCode != StatusCodes.Status404NotFound)
+            {
+                response = "Exists";
+            }
+
+            Request.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            return response;
+        }
+        
+        // POST: api/Domains
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Domains/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
